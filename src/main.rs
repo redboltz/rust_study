ee3806293b0b8980c7ce3452f73a979ff85b46c9
Takatoshi Kondo <redboltz@gmail.com>
2019-12-18 19:07:32 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-#![allow(unused_variables)]
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    let r;
-    {
-        let x = 5;            // ----------+-- 'b
-                              //           |
-        r = &x;               // --+-- 'a  |
-                              //   |       |
-        println!("r: {}", r); //   |       |
-                              // --+       |
-    }                         // ----------+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-
+    let result = longest(string1.as_str(), string2);
+    // 最長の文字列は、{}です
+    println!("The longest string is {}", result);
 }
