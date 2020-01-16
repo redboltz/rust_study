07148c2b2d35f27cd1e16d85261c406a8c38bf09
Takatoshi Kondo <redboltz@gmail.com>
2020-01-16 10:13:40 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -26,23 +26,30 @@ mod tests {
     use super::*;
     
     #[test]
-    fn tc1() {
+    fn assert_eq_requires_Debug_trait() {
         let my1 = My {m1: 1, m2: 2};
         let my2 = My {m1: 3, m2: 4};
         assert_eq!(my1, my2);
     }
 
     #[test]
-    fn tc2() {
+    fn assert_doesnt_use_trait_automatically() {
         let my1 = My {m1: 1, m2: 2};
         let my2 = My {m1: 3, m2: 4};
         assert!(my1 == my2);
     }
 
     #[test]
-    fn tc3() {
+    fn custom_print_using_Display_trait() {
         let my1 = My {m1: 1, m2: 2};
         let my2 = My {m1: 3, m2: 4};
         assert!(my1 == my2, "{} != {}", my1, my2);
+    }
+
+    #[test]
+    fn custom_print_using_Debug_trait() {
+        let my1 = My {m1: 1, m2: 2};
+        let my2 = My {m1: 3, m2: 4};
+        assert!(my1 == my2, "{:?} != {:?}", my1, my2);
     }
 }
